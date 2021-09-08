2fd384f63c130bbaf2c540e1a9ea715d171ade9d
Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>
2021-09-08 08:33:47 +0200

    Restore scripts, docker images and jenkins
    Signed-off-by: Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>

--- docker/run_mailtck.sh
+++ docker/run_mailtck.sh
@@ -15,26 +15,62 @@
 #
 # SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
 
+if ls ${WORKSPACE}/bundles/*mail-tck*.zip 1> /dev/null 2>&1; then
+  unzip ${WORKSPACE}/bundles/*mail-tck*.zip -d ${WORKSPACE}
+else
+  echo "[ERROR] TCK bundle not found"
+  exit 1
+fi
+
 if [[ "$JDK" == "JDK11" || "$JDK" == "jdk11" ]];then
   export JAVA_HOME=${JDK11_HOME}
   export PATH=$JAVA_HOME/bin:$PATH
 fi
+  
+export TS_HOME=${WORKSPACE}/mail-tck
 
-sed -i "s#^JAVAMAIL_SERVER=.*#JAVAMAIL_SERVER=localhost -pn 1143#g" "lib/ts.properties"
-sed -i "s#^JAVAMAIL_PROTOCOL=.*#JAVAMAIL_PROTOCOL=imap#g" "lib/ts.properties"
-sed -i "s#^JAVAMAIL_TRANSPORT_PROTOCOL=.*#JAVAMAIL_TRANSPORT_PROTOCOL=smtp#g" "lib/ts.properties"
-sed -i "s#^JAVAMAIL_TRANSPORT_SERVER=.*#JAVAMAIL_TRANSPORT_SERVER=localhost -tpn 1025#g" "lib/ts.properties"
-sed -i "s#^JAVAMAIL_USERNAME=.*#JAVAMAIL_USERNAME=$MAIL_USER#g" "lib/ts.properties"
-sed -i "s#^JAVAMAIL_PASSWORD=.*#JAVAMAIL_PASSWORD=1234#g" "lib/ts.properties"
-sed -i "s#^SMTP_DOMAIN=.*#SMTP_DOMAIN=james.local#g" "lib/ts.properties"
-sed -i "s#^SMTP_FROM=.*#SMTP_FROM=user01@james.local#g" "lib/ts.properties"
-sed -i "s#^SMTP_TO=.*#SMTP_TO=user01@james.local#g" "lib/ts.properties"
+sed -i "s#^TS_HOME=.*#TS_HOME=$TS_HOME#g" "$TS_HOME/lib/ts.jte"
+sed -i "s#^JAVA_HOME=.*#JAVA_HOME=$JAVA_HOME#g" "$TS_HOME/lib/ts.jte"
+sed -i "s#^JARPATH=.*#JARPATH=$WORKSPACE#g" "$TS_HOME/lib/ts.jte"
+sed -i "s#^JAVAMAIL_SERVER=.*#JAVAMAIL_SERVER=localhost -pn 1143#g" "$TS_HOME/lib/ts.jte"
+sed -i "s#^JAVAMAIL_PROTOCOL=.*#JAVAMAIL_PROTOCOL=imap#g" "$TS_HOME/lib/ts.jte"
+sed -i "s#^JAVAMAIL_TRANSPORT_PROTOCOL=.*#JAVAMAIL_TRANSPORT_PROTOCOL=smtp#g" "$TS_HOME/lib/ts.jte"
+sed -i "s#^JAVAMAIL_TRANSPORT_SERVER=.*#JAVAMAIL_TRANSPORT_SERVER=localhost -tpn 1025#g" "$TS_HOME/lib/ts.jte"
+sed -i "s#^JAVAMAIL_USERNAME=.*#JAVAMAIL_USERNAME=$MAIL_USER#g" "$TS_HOME/lib/ts.jte"
+sed -i "s#^JAVAMAIL_PASSWORD=.*#JAVAMAIL_PASSWORD=1234#g" "$TS_HOME/lib/ts.jte"
+sed -i "s#^SMTP_DOMAIN=.*#SMTP_DOMAIN=james.local#g" "$TS_HOME/lib/ts.jte"
+sed -i "s#^SMTP_FROM=.*#SMTP_FROM=user01@james.local#g" "$TS_HOME/lib/ts.jte"
+sed -i "s#^SMTP_TO=.*#SMTP_TO=user01@james.local#g" "$TS_HOME/lib/ts.jte"
 
-which mvn
-mvn -version
+mkdir -p ${HOME}/.m2
 
-mvn clean install -DskipTests
-java -jar target/jakarta.mail-tck-1.0.0-SNAPSHOT.jar -f -s target/test-classes/mailboxes/test1 -d imap://user01%40james.local:1234@localhost:1143/test1
-mvn test
+WGET_PROPS="--progress=bar:force --no-cache"
+if [ -z "$JAF_BUNDLE_URL" ];then
+  export JAF_BUNDLE_URL=https://repo1.maven.org/maven2/com/sun/activation/jakarta.activation/2.0.0/jakarta.activation-2.0.0.jar
+fi
+if [ -z "$MAIL_BUNDLE_URL" ];then
+  export MAIL_BUNDLE_URL=https://repo1.maven.org/maven2/com/sun/mail/jakarta.mail/2.0.0/jakarta.mail-2.0.0.jar
+fi
+wget $WGET_PROPS $JAF_BUNDLE_URL -O jakarta.activation.jar
+wget $WGET_PROPS $MAIL_BUNDLE_URL -O jakarta.mail.jar
 
-# FIXME report directory?
+cd $TS_HOME/tests/mailboxes
+export CLASSPATH=$TS_HOME/tests/mailboxes:$WORKSPACE/jakarta.mail.jar:$WORKSPACE/jakarta.activation.jar:$CLASSPATH
+javac -cp $CLASSPATH fpopulate.java
+java -cp $CLASSPATH fpopulate -s test1 -d imap://user01%40james.local:1234@localhost:1143
+
+which ant
+ant -version
+
+cd $WORKSPACE/mail-tck/
+ant -Dreport.dir=$WORKSPACE/JTreport/mail-tck -Dwork.dir=$WORKSPACE/JTwork/mail-tck run
+
+
+HOST=`hostname -f`
+echo "1 mail-tck $HOST" > $WORKSPACE/args.txt
+
+mkdir -p $WORKSPACE/results/junitreports/
+JT_REPORT_DIR=$WORKSPACE/JTreport
+$JAVA_HOME/bin/java -Djunit.embed.sysout=true -jar ${WORKSPACE}/docker/JTReportParser/JTReportParser.jar $WORKSPACE/args.txt $JT_REPORT_DIR $WORKSPACE/results/junitreports/ 
+
+tar zcvf ${WORKSPACE}/mail-tck-results.tar.gz $WORKSPACE/JTreport/mail-tck $WORKSPACE/JTwork/mail-tck $WORKSPACE/results/junitreports/
